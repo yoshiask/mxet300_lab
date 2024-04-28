1393178f757fb34c48ccf5f5a71f569ec4bc5cce
Joshua Askharoun <jjask7@gmail.com>
2024-04-27 20:59:19 -0500

    Merge avoidance logic

--- basics/L3_runaway_alarm.py
+++ basics/L3_runaway_alarm.py
@@ -3,10 +3,12 @@
 
 import cv2              # For image capture and processing
 import numpy as np      
+import L1_motor as lm
+import L1_lidar as lidar
 import L2_speed_control as sc
 import L2_inverse_kinematics as ik
 import L2_kinematics as kin
-import netifaces as ni
+import L2_vector as lv
 from time import sleep
 from math import radians, pi
 
@@ -58,122 +60,132 @@ def getInitialBounds(img):
     return roi
 
 
-initialBounds = None
-while initialBounds is None:
-    success, img = cap.read()
-    if not success:
-        print("Failed to read from camera!")
-        continue
-    initialBounds = getInitialBounds(img)
-    print("Initial target not found!")
-    print("Trying again in 5 seconds...")
-    sleep(5)
-
-print("Starting bounds:", initialBounds)
-Tracker.init(img, initialBounds)
-
-while True:
-    Timer = cv2.getTickCount()
-    success, img = cap.read()
-
-    success, bound = Tracker.update(img)
-
-    frame_height, frame_width, _ = img.shape
-    x, y, w, h = bound
-
-    if ((frame_height - x) < EDGE_MARGIN) or ((frame_width - y) < EDGE_MARGIN) \
-        or (x < EDGE_MARGIN) or (y < EDGE_MARGIN):
-        # The bounds are really close to the edge,
-        # so we'll assume we lost the object.
-        success = False
-
-    if success:
-        # Human detected!
-        x,y,w,h = bound                                             # Get bounding rectangle (x,y,w,h) of the target
-        center_x, center_y = (int(x+0.5*w), int(y+0.5*h))           # defines center of rectangle around the largest target area
-        angle = round(((center_x / frame_width) - 0.5) * FOV, 3)    # angle of vector towards target center from camera, where 0 deg is centered
-
-        print(f"Human found at {center_x}, {center_y}!")
-
-        wheel_measured: np.array = kin.getPdCurrent()           # Wheel speed measurements
-
-        # Always move away from the target
-        fwd_effort = 2
-        
-        wheel_speed = ik.getPdTargets(np.array([0.8*fwd_effort, -0.5*angle]))   # Find wheel speeds for approach and heading correction
-        sc.driveClosedLoop(wheel_speed, wheel_measured, 0)  # Drive closed loop
-        print("Angle: ", angle, " | Target L/R: ", *wheel_speed, " | Measured L\R: ", *wheel_measured)
+def getAngleCategory(angle):
+    if 180 >= angle > 130:
+        return "slightly left"
+    elif 130 >= angle > 80:
+        return "left"
+    elif 80 >= angle > 30:
+        return "hard left"
+    elif 30 >= angle >= 0:
+        return "pivot left"
+    elif -180 <= angle < -130:
+        return "slightly right"
+    elif -130 <= angle < -80:
+        return "right"
+    elif -80 <= angle < -30:
+        return "hard right"
+    elif -30 <= angle <= 0:
+        return "pivot right"
     else:
-        # SPIIIIIIIN!!!
-        print("No targets")
-        sc.driveOpenLoop(np.array([+4.0, -4.0]))
-        restartTracking(img)
+        return "straight"
+
+
+def avoidObstacles(distance, angle):
+    if 0.08 <= distance < 0.5:  # If the distance is less than 0.5 meters
+        angle_category = getAngleCategory(angle)
+        print(f"Distance: {distance}, Angle: {angle}, Action: {angle_category}")
+
+        if angle_category == "slightly left":
+            lm.sendLeft(1.0)
+            lm.sendRight(0.8)
+        elif angle_category == "left":
+            lm.sendLeft(1.0)
+            lm.sendRight(0.6)
+        elif angle_category == "hard left":
+            lm.sendLeft(1.0)
+            lm.sendRight(0.4)
+        elif angle_category == "pivot left":
+            lm.sendLeft(1.0)
+            lm.sendRight(0.0)
+        elif angle_category == "slightly right":
+            lm.sendLeft(0.8)
+            lm.sendRight(1.0)
+        elif angle_category == "right":
+            lm.sendLeft(0.6)
+            lm.sendRight(1.0)
+        elif angle_category == "hard right":
+            lm.sendLeft(0.4)
+            lm.sendRight(1.0)
+        elif angle_category == "pivot right":
+            lm.sendLeft(0.0)
+            lm.sendRight(1.0)
+        return True
+    elif distance < 0.0799999:
+        lm.sendLeft(-0.8)
+        lm.sendRight(-0.8)
+        sleep(2)
+        lm.sendLeft(0.0)
+        lm.sendRight(0.8)
+        sleep(2)
+        return True
+    return False
 
 
 def main():
-    # Try opening camera with default method
-    try:
-        camera = cv2.VideoCapture(0)    
-    except: pass
+    initialBounds = None
+    img = None
+    while initialBounds is None:
+        success, img = cap.read()
+        if not success:
+            print("Failed to read from camera!")
+            continue
+        initialBounds = getInitialBounds(img)
+        print("Initial target not found!")
+        print("Trying again in 0.5 seconds...")
+        sleep(0.5)
 
-    # Try opening camera stream if default method failed
-    if not camera.isOpened():
-        camera = cv2.VideoCapture(camera_input)    
+    print("Starting bounds:", initialBounds)
+    Tracker.init(img, initialBounds)
 
-    #camera.set(3, size_w)                       # Set width of images that will be retrived from camera
-    #camera.set(4, size_h)                       # Set height of images that will be retrived from camera
+    while True:
+        # Get LIDAR measurements to map the room
+        ob_distance, ob_angle = lv.getNearest()
+        print(f"Obstacle (r={ob_distance}, θ={ob_angle})")
 
-    Tracker = cv2.legacy.TrackerCSRT_create()
-    success, img = camera.read()
-    bound = cv2.selectROI("Tracking...", img, False)
-    Tracker.init(img,bound)
+        # First check for obstacles. If we're about to crash into something,
+        # it doesn't matter if the human is right behind us.
+        obstacleAvoided = avoidObstacles(ob_distance, ob_angle)
+        if obstacleAvoided:
+            sleep(0.1)
+            continue
 
-    try:
-        while True:
-            sleep(.05)                                          
+        Timer = cv2.getTickCount()
+        success, img = cap.read()
 
-            Timer = cv2.getTickCount()
-            success, img = camera.read()
-            success, bound = Tracker.update(img)
+        foundHuman, bound = Tracker.update(img)
 
-            if success:
-                drawBox(img, bound)
-            else: 
-                cv2.putText(img, "LOST", (75,75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+        frame_height, frame_width, _ = img.shape
+        x, y, w, h = bound
 
-            cv2.imshow("Tracking...", img)
-            fps = cv2.getTickFrequency()/(cv2.getTickCount()-Timer)
-            cv2.putText(img, str(int(fps)), (75,50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+        if ((frame_height - x) < EDGE_MARGIN) or ((frame_width - y) < EDGE_MARGIN) \
+            or (x < EDGE_MARGIN) or (y < EDGE_MARGIN):
+            # The bounds are really close to the edge,
+            # so we'll assume we lost the object.
+            foundHuman = False
+
+        if foundHuman:
+            # Human detected!
+            x,y,w,h = bound                                             # Get bounding rectangle (x,y,w,h) of the target
+            center_x, center_y = (int(x+0.5*w), int(y+0.5*h))           # defines center of rectangle around the largest target area
+            angle = round(((center_x / frame_width) - 0.5) * FOV, 3)    # angle of vector towards target center from camera, where 0 deg is centered
+
+            print(f"Human found at {center_x}, {center_y}!")
+
+            wheel_measured: np.array = kin.getPdCurrent()           # Wheel speed measurements
+
+            # Always move away from the target
+            fwd_effort = 2
             
-            if success:
-                # Human detected!
-                c = max(cnts, key=cv2.contourArea)                      # return the largest target area
-                x,y,w,h = cv2.boundingRect(c)                           # Get bounding rectangle (x,y,w,h) of the largest contour
-                center = (int(x+0.5*w), int(y+0.5*h))                   # defines center of rectangle around the largest target area
-                angle = round(((center[0]/width)-0.5)*fov, 3)           # angle of vector towards target center from camera, where 0 deg is centered
+            wheel_speed = ik.getPdTargets(np.array([0.8*fwd_effort, -0.5*angle]))   # Find wheel speeds for approach and heading correction
+            sc.driveClosedLoop(wheel_speed, wheel_measured, 0)  # Drive closed loop
+            print("Angle: ", angle, " | Target L/R: ", *wheel_speed, " | Measured L\R: ", *wheel_measured)
+        else:
+            # SPIIIIIIIN!!!
+            print("No targets")
+            sc.driveOpenLoop(np.array([+4.0, -4.0]))
+            restartTracking(img)
 
-                wheel_measured: np.array = kin.getPdCurrent()           # Wheel speed measurements
-
-                # Always move away from the target
-                fwd_effort = 2
-                
-                wheel_speed = ik.getPdTargets(np.array([0.8*fwd_effort, -0.5*angle]))   # Find wheel speeds for approach and heading correction
-                sc.driveClosedLoop(wheel_speed, wheel_measured, 0)  # Drive closed loop
-                print("Angle: ", angle, " | Target L/R: ", *wheel_speed, " | Measured L\R: ", *wheel_measured)
-            else:
-                # TODO: SPIIIIIIIN!!!
-                print("No targets")
-                sc.driveOpenLoop(np.array([+4.0, -4.0]))
-            
-            if cv2.waitKey(1) & 0xff ==ord('q'):
-                break
-
-                
-    except KeyboardInterrupt: # condition added to catch a "Ctrl-C" event and exit cleanly
-        pass
-
-    finally:
-    	print("Exiting Color Tracking.")
 
 if __name__ == '__main__':
     main()
